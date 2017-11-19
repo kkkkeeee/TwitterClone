d921e391529f54d59782fb5b47bc933861215634
kkkkeeee <zhaikeke1@gmail.com>
2017-11-18 20:25:30 -0500

    added function subscribe such that the user can see the newly posted tweet by the users it follows

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,9 +2,12 @@ defmodule App.UserController do
   use App.Web, :controller
 
   alias App.Tweet
+  alias App.Retweet
   alias App.User
+  alias App.Follower
 
   import Ecto.Changeset
+  @num_tweets 5
 
   plug App.LoginRequired when action in [:edit, :update]
   plug App.SetUser when action in [:show]
@@ -17,7 +20,38 @@ defmodule App.UserController do
   def show(conn, _params) do
     user = conn.assigns[:user]
     changeset = Tweet.changeset %Tweet{}
-    render conn, "show.html", user: user, changeset: changeset
+    #added by keke to show the tweet posted by the user this current user is following
+    #tweets = 
+    #       Tweet |> join(:inner, [t], f in Follower, f.follower_id == ^user.id and t.user_id == f.user_id)             
+    #tweets = Repo.all(tweets) |> Repo.preload(:user)
+    #select * from(
+    #  select * from tweets t where t.user_id = $1
+    #  union
+    query = """ 
+          select * from
+          (select t.* from tweets t, followers f
+          where f.follower_id = $1 and t.user_id = f.user_id 
+          and t.inserted_at > (select max(t2.inserted_at) 
+                               from tweets t2
+                               where t2.user_id = $1)
+          order by t.inserted_at desc) s
+          limit $2
+    """
+    res = Ecto.Adapters.SQL.query!(App.Repo, query, [user.id, @num_tweets])
+    cols = Enum.map res.columns, &(String.to_atom(&1)) # b
+    
+    tweets = Enum.map res.rows, fn(row) ->
+      struct(App.Tweet, Enum.zip(cols, row)) # c
+    end
+    tweets = tweets |> Repo.preload(:user)
+    #tweets = Tweet.changeset(%Tweet{}, tweets)
+    #IO.puts(", tweets: #{inspect tweets}")
+    #query = Tweet|> join(:inner, [t], u in User, (u.id == ^user.id or u.id == (from f in Follower, where f.follower_id == ^user.id, select f.user_id)) 
+    #          and t.user_id == u.id) 
+    #tweets = Repo.all(query)
+    #end added by keke
+
+    render conn, "show.html", user: user, changeset: changeset, tweets: tweets #added by keke
   end
 
   def edit(conn, %{"id" => id}) do
